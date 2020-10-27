1f91a6459fdbefc6f8f616f40da41662511dc2ce
chetan pawar <chetan882777@gmail.com>
2020-10-27 14:51:31 +0530

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,37 +14,52 @@ class MyApp extends StatelessWidget {
           title: Text("My App"),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextWidget("Hello"),
-              SizedBox(height: 8,),
-              TextWidget("World"),
-              SizedBox(height: 8,),
-              TextWidget("How are you?"),
-            ],
-          )
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextWidget("Hello"),
+            SizedBox(
+              height: 8,
+            ),
+            TextWidget("World"),
+            SizedBox(
+              height: 8,
+            ),
+            TextWidget("How are you?"),
+          ],
+        )),
       ),
     );
   }
 }
 
-class TextWidget extends StatelessWidget {
-
+class TextWidget extends StatefulWidget {
   final String name;
 
   const TextWidget(this.name);
 
   @override
+  State<StatefulWidget> createState() => _TextWidgetState();
+}
+
+class _TextWidgetState extends State<TextWidget> {
+  int count = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(color: Colors.blueAccent),
-      child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Text(name),
-      ),
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: DecoratedBox(
+          decoration: BoxDecoration(color: Colors.blueAccent),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("${widget.name} - $count"),
+          ),
+        )
     );
   }
-
 }
